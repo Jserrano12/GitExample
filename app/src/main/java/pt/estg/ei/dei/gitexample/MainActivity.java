465a28ceff090e8f91ee669a49de9e26babfb588
joaos <joaoserrano.31@gmail.com>
2016-09-27 16:44:01 +0100

    New activity .

--- app/src/main/java/pt/estg/ei/dei/gitexample/MainActivity.java
+++ app/src/main/java/pt/estg/ei/dei/gitexample/MainActivity.java
@@ -1,13 +1,24 @@
 package pt.estg.ei.dei.gitexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnNextPage(View view) {
+        Intent i = new Intent(this, Main2Activity.class);
+        Button nomeParaOBotao = (Button) findViewById(R.id.button2);
+        startActivity(i);
     }
 }
